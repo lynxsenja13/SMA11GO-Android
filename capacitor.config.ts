77963d0867021f89e608b795ebdb7b920e7a6c3a
lynxsenja13 <lynxsenja13@gmail.com>
2026-09-07 23:46:27 +0700

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -5,18 +5,16 @@ const config: CapacitorConfig = {
   appName: 'SMA11GO',
   webDir: 'www',
 
-  server: {
-    url: 'https://sma11go.site.je/',
-    cleartext: false
-  },
-
   plugins: {
     SplashScreen: {
       launchAutoHide: true,
-      launchShowDuration: 700,
+      launchShowDuration: 300,
+      launchFadeOutDuration: 200,
       backgroundColor: '#0d47a1',
       showSpinner: false,
-      androidScaleType: 'CENTER_INSIDE'
+      androidScaleType: 'CENTER_INSIDE',
+      splashFullScreen: true,
+      splashImmersive: true
     }
   }
 };
